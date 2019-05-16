2cffc410ee025f9de2e39a91944922a2421b35b3
Stefan Stefanov <stefan.bg@gmail.com>
2019-05-16 20:22:29 +0300

    Step 02 - simple model

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,73 @@
 import express from 'express';
 import graphqlHTTP from 'express-graphql';
 import { buildSchema } from 'graphql';
+import data from './data';
 
 const app = express();
 
 const schema = buildSchema(`
+    """Demo app root query fields"""
     type Query {
-        hello(what: String = "World"): String
+        """All users in database"""
+        users: [User!]!
+
+        """All posts in database"""
+        posts: [Post!]!
+
+        """User by ID"""
+        user("""User ID""" id: ID!): User!
+
+        """Post by ID"""
+        post("""Post ID""" id: ID!): Post!
+    }
+
+    type User {
+        id: ID!
+        name: String!
+        email: String
+        posts: [Post!]!
+    }
+
+    type Post {
+        id: ID!
+        title: String!
+        body: String!
+        author: User
     }
 `);
+
+function getUserById(id) {
+    return data.users.find(user => user.id == id)
+}
+
+function getPostById(id) {
+    return data.posts.find(post => post.id == id)
+}
+
+data.users = data.users.map(user => {
+    return {
+        ...user,
+        posts: () => data.posts.filter(post => post.authorId == user.id)
+    }
+});
+
+data.posts = data.posts.map(post => {
+    return {
+        ...post,
+        author: () => {
+            console.log(getUserById(post.authorId));
+            return getUserById(post.authorId);
+        }
+    }
+});
 
 app.use(graphqlHTTP({
     schema,
     rootValue: {
-        hello: ({ what }: any) => {
-            return what;
-        }
+        users: data.users,
+        posts: data.posts,
+        post: ({ id }) => getPostById(id),
+        user: ({ id }) => getUserById(id),
     },
     graphiql: true
 }));
