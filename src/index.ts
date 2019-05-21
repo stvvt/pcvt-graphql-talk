9b54acad46b99bd5e0cf7abf68668a549881a070
Stefan Stefanov <stefan.bg@gmail.com>
2019-05-21 14:33:40 +0300

    Proto scrapper

--- src/index.ts
+++ src/index.ts
@@ -1,63 +1,199 @@
 import { ApolloServer, gql } from 'apollo-server';
-import data from './data';
-import Orm from './orm';
+import fs from 'fs';
+import puppeteer, { Browser } from 'puppeteer';
 
-const orm = new Orm(data);
+class Element {
+    constructor(private page: puppeteer.Page, private elementHandle: puppeteer.ElementHandle) { }
 
-const typeDefs = gql`
-    """ Demo app root query fields """
-    type Query {
-        """All users in database"""
-        users: [User!]!
-
-        """ All posts in database"""
-        posts: [Post!]!
-
-        """User by ID"""
-        user("""User ID""" id: ID!): User!
-
-        """Post by ID"""
-        post(id: ID!): Post!
+    public async attr(name: string) {
+        const value = await this.page.evaluate((element: HTMLElement, name) => {
+            return element ? element.getAttribute(name) : null;
+        }, this.elementHandle, name);
+        return value;
     }
 
-    """Represents an user of the app"""
-    type User {
-        id: ID!
-        name: String!
-        email: String
-        posts: [Post!]!
+    public async prop(name: string) {
+        const value = await this.page.evaluate((element: HTMLElement, name) => {
+            return element ? element[name] : null;
+        }, this.elementHandle, name);
+        return value;
     }
 
-    """Represents a blog post"""
-    type Post {
-        id: ID!
-        title: String!
-        body: String!
-        author: User
+    public async text() {
+        const value = await this.page.evaluate((element: HTMLElement) => element ? element.innerText : null, this.elementHandle);
+        return value;
     }
-`;
 
-const users = orm.models.users;
-const posts = orm.models.posts;
+    public async attrOf(name: string, selector?: string) {
+        const el = selector ? await this.child(selector) : this;
+        return el.attr(name);
+    }
 
-const resolvers = {
+    public async propOf(name: string, selector?: string) {
+        const el = selector ? await this.child(selector) : this;
+        return el.prop(name);
+    }
+
+    public async textOf(selector?: string) {
+        const el = selector ? await this.child(selector) : this;
+        return el.text();
+    }
+
+    public async countOf(itemSelector: string, selector?: string) {
+        const el = selector ? await this.child(selector) : this;
+        const elementHandles = await el.elementHandle.$$(itemSelector);
+
+        return elementHandles.length;
+    }
+
+    public async child(selector: string) {
+        const elementHandle = await this.elementHandle.$(selector);
+        return new Element(this.page, elementHandle);
+    }
+
+    public async children(selector: string) {
+        const elementHandles = await this.elementHandle.$$(selector);
+        return elementHandles.map(elementHandle => new Element(this.page, elementHandle));
+    }
+}
+
+interface Context {
+    browser: () => Browser;
+}
+
+interface QueryResolvers {
+    child: (_: any, args: { url: string, selector: string }, ctx: Context) => Promise<Element>;
+    children: (_: any, args: { url: string, selector: string }, ctx: Context) => Promise<Element[]>;
+}
+
+interface ElementResolvers {
+    attrOf: (element: Element, args: { selector?: string, name: string }, ctx: Context) => Promise<string>;
+    propOf: (element: Element, args: { selector?: string, name: string }, ctx: Context) => Promise<string>;
+    textOf: (element: Element, args: { selector?: string }, ctx: Context) => Promise<string>;
+    countOf: (element: Element, args: { selector?: string, itemSelector: string }, ctx: Context) => Promise<number>;
+    child: (element: Element, args: { selector: string }, ctx: Context) => Promise<Element>;
+    children: (element: Element, args: { selector: string }, ctx: Context) => Promise<Element[]>;
+    link: (element: Element, args: { selector: string, urlType: string, name: string, itemSelector: string }, ctx: Context) => Promise<Element>;
+}
+
+interface Resolvers {
+    Query: QueryResolvers;
+    Element: ElementResolvers;
+}
+
+const createdPages: puppeteer.Page[] = []
+
+async function createPage({browser}) {
+    const page: puppeteer.Page = await (await browser()).newPage();
+
+    page.on('console', msg => console.log('>>>', msg.text()));
+
+    await page.setRequestInterception(true);
+
+    page.on('request', (req) => {
+        if (req.resourceType() == 'stylesheet' || req.resourceType() == 'font' || req.resourceType() == 'image') {
+            req.abort();
+        }
+        else {
+            req.continue();
+        }
+    });
+
+    createdPages.push(page);
+
+    return page;
+}
+
+const resolvers: Resolvers = {
     Query: {
-        users: () => users.all(),
-        posts: () => posts.all(),
-        user: (_, { id }) => users.find('id', id),
-        post: (_, { id }) => posts.find('id', id)
+        child: async (_, { url, selector }, ctx) => {
+            const page = await createPage(ctx);
+            await page.goto(url);
+            const documentHandle = await page.waitForSelector(selector);
+            return new Element(page, documentHandle);
+        },
+        children: async (_, { url, selector }, ctx) => {
+            const page = await createPage(ctx);
+            await page.goto(url);
+            await page.waitForSelector(selector);
+            const documentHandles = await page.$$(selector);
+            return documentHandles.map(documentHandle => new Element(page, documentHandle));
+        },
     },
-    User: {
-        posts: (user) => posts.filter('authorId', user.id)
-    },
-    Post: {
-        author: (post) => users.find('id', post.authorId)
+    Element: {
+        attrOf: async (element, { selector, name }) => {
+            return element.attrOf(name, selector);
+        },
+        propOf: async (element, { selector, name }) => {
+            return element.propOf(name, selector);
+        },
+        textOf: async (element, { selector }) => {
+            return element.textOf(selector);
+        },
+        countOf: async (element, { selector, itemSelector }) => {
+            return element.countOf(itemSelector, selector);
+        },
+        child: async (element, { selector }) => {
+            return element.child(selector);
+        },
+        children: async (element, { selector }) => {
+            return element.children(selector);
+        },
+        link: async (element, { selector, urlType, name, itemSelector }, ctx) => {
+            let url: string;
+
+            switch (urlType) {
+                case 'Attr':
+                    url = await element.attrOf(name, selector);
+                    break;
+                case 'Prop':
+                    url = await element.propOf(name, selector);
+                    break;
+                case 'Text':
+                    url = await element.textOf(selector);
+                    break;
+            }
+
+            const page = await createPage(ctx);
+
+            await page.goto(url);
+            const documentHandle = await page.waitForSelector(itemSelector);
+            return new Element(page, documentHandle);
+        }
     }
 };
 
+function getBrowserFactory() {
+    let browser: puppeteer.Browser = null;
+    return async function getBrowser() {
+        if (browser === null) {
+            browser = await puppeteer.launch({
+                headless: false
+            });
+        }
 
-const server = new ApolloServer({ typeDefs, resolvers });
+        return browser;
+    }
+}
 
-server.listen().then(({ url }) => {
+async function main() {
+    const typeDefs = gql`${fs.readFileSync(__dirname + '/schema.graphql')}`;
+    const browserFactory = getBrowserFactory();
+    const server = new ApolloServer({
+        typeDefs,
+        resolvers: resolvers as any,
+        context: ({ res }) => {
+            res.on('finish', () => {
+                createdPages.forEach((page, index) => {
+                    page.close().then(() => delete createdPages[index]);
+                });
+            });
+            return { browser: browserFactory };
+        }
+    });
+
+    const { url } = await server.listen();
     console.log(`🚀  Server ready at ${url}`);
-});
+}
+
+main();
